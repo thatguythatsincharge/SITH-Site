9285160e2e75c5d1863f2c35e43954d9af9cf1c0
thatguythatsincharge <36935809+thatguythatsincharge@users.noreply.github.com>
2018-09-16 22:12:23 -0700

    pushing to heroku, did a lot!

--- server.js
+++ server.js
@@ -69,6 +69,10 @@ app.get('/star_wars_episode/:episode_number?', function(req, res) {
 	res.render("movie_single", {movies: moviesArray, currentMovie: currentMovie });
 });
 
-app.listen(3000, function() {
-	console.log('The application is running on port 3000');
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+	console.log('The application is running on the port');
 });
